2f3d765706ed0a63d455b18cce07c2fd832bd689
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-03-11 10:39:27 -0300

    Criação do Domain e da Infrastructure.

---
+++ Infrastucture/Repository/Repositories/RepositoryMessage.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastucture.Repository.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.Repository.Repositories
+{
+    public class RepositoryMessage : RepositoryGenerics<Message>, IMessage
+    {
+        public RepositoryMessage()
+        {
+            
+        }
+    }
+}
